0e084c6684a8b4f518ac3f3c6293a9432f695c4b
meursong <feelssu@gmail.com>
2025-01-05 16:29:44 +0900

    Add XapiVerbUtil for XapiVerb creation
    Implemented a utility class for creating XapiVerb objects using various input types like XapiVerbEnum or custom URI with optional display mapping. This utility centralizes common creation logic, promoting code reusability and consistency.

---
+++ src/main/java/com/meursong/xapi/util/XapiVerbUtil.java
@@ -0,0 +1,29 @@
+package com.meursong.xapi.util;
+
+import com.meursong.xapi.consonant.Language;
+import com.meursong.xapi.consonant.XapiVerbEnum;
+import com.meursong.xapi.dto.element.xapiVerb.XapiVerb;
+
+import java.util.Map;
+
+public class XapiVerbUtil {
+
+    private XapiVerbUtil() {}
+
+    // Creates new XapiVerb object with XapiVerbEnum
+    public static XapiVerb createVerbWithVerbEnum(XapiVerbEnum verbType) {
+        return new XapiVerb(verbType.getId());
+    }
+
+    // Creates new XapiVerb object with the given id(uri).
+    public static XapiVerb createVerb(String id) {
+        XapiUtil.validateUri(id);
+        return new XapiVerb(id);
+    }
+
+    // Creates new XapiVerb object with the given id(uri) and display.
+    public static XapiVerb createVerb(String id, Map<Language, String> display) {
+        XapiUtil.validateUri(id);
+        return new XapiVerb(id, display);
+    }
+}
